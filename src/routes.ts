2d108bf745b7f46c4574756780589cc275ea8e58
Victor Costa Melo <victor.costa.melo@hotmail.com>
2020-06-22 23:59:23 -0300

    mais ajustes no body do create

--- src/routes.ts
+++ src/routes.ts
@@ -25,12 +25,13 @@ routes.post(
   celebrate({
     body: Joi.object().keys({
       name: Joi.string().required(),
+      email: Joi.string().required().email(),
       whatsapp: Joi.number().required(),
       latitude: Joi.number().required(),
       longitude: Joi.number().required(),
       city: Joi.string().required(),
       uf: Joi.string().required().max(2),
-      item: Joi.string().required(),
+      items: Joi.string().required(),
     })
   }, {
     abortEarly: false

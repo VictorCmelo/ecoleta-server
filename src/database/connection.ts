017c91e1d30843f5a23286c75c88d9ce382ad942
Victor Costa Melo <victor.costa.melo@hotmail.com>
2020-06-27 13:37:04 -0300

    desfazendo

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -5,8 +5,8 @@ const connection = knex({
   client:'pg',
   connection:{
     database: "ecoleta",
-    user:"gmowfwbqtlbyon",
-    password: "1b4c0b9f95ba164c54deebf5a97355e02371816a8dacc354c748cfe86fc09cc1"
+    user:"postgres",
+    password: "docker"
    
   },
   useNullAsDefault:true,

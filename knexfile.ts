2f52eea019047a3075f18f92314c6fa9a54c9a1a
Victor Costa Melo <victor.costa.melo@hotmail.com>
2020-06-27 13:01:06 -0300

    teste bd production heroku

--- knexfile.ts
+++ knexfile.ts
@@ -1,18 +1,36 @@
-import path from 'path';
+import path from "path";
 
 module.exports = {
-  client:'pg',
-  connection:{
-    database: "ecoleta",
-    user:"postgres",
-    password: "docker"
+  development: {
+    client: "pg",
+    connection: {
+      database: "ecoleta",
+      user: "postgres",
+      password: "docker",
+    },
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "migrations"),
+    },
+    seeds: {
+      directory: path.resolve(__dirname, "src", "database", "seeds"),
+    },
+
+    useNullAsDefault: true,
   },
-  migrations: {
-    directory: path.resolve(__dirname, 'src', 'database', 'migrations')
-  },
-  seeds: {
-    directory: path.resolve(__dirname, 'src', 'database', 'seeds')
-  },
-  
-  useNullAsDefault:true,
+  production: {
+    client: "pg",
+    connection: {
+      database: "ecoleta",
+      user: "gmowfwbqtlbyon",
+      password: "1b4c0b9f95ba164c54deebf5a97355e02371816a8dacc354c748cfe86fc09cc1",
+    },
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "migrations"),
+    },
+    seeds: {
+      directory: path.resolve(__dirname, "src", "database", "seeds"),
+    },
+
+    useNullAsDefault: true,
+  }
 };
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -5,8 +5,8 @@ const connection = knex({
   client:'pg',
   connection:{
     database: "ecoleta",
-    user:"postgres",
-    password: "docker"
+    user:"gmowfwbqtlbyon",
+    password: "1b4c0b9f95ba164c54deebf5a97355e02371816a8dacc354c748cfe86fc09cc1"
    
   },
   useNullAsDefault:true,

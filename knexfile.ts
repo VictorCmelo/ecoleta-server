37de9758e94b501745cf99f7e09d660fc1045f50
Victor Costa Melo <victor.costa.melo@hotmail.com>
2020-06-27 13:38:12 -0300

    merge

--- knexfile.ts
+++ knexfile.ts
@@ -4,13 +4,8 @@ module.exports = {
   client:'pg',
   connection:{
     database: "ecoleta",
-<<<<<<< HEAD
-    user:"gmowfwbqtlbyon",
-    password: "1b4c0b9f95ba164c54deebf5a97355e02371816a8dacc354c748cfe86fc09cc1"
-=======
     user:"postgres",
     password: "docker"
->>>>>>> parent of 2f52eea... teste bd production heroku
   },
   migrations: {
     directory: path.resolve(__dirname, 'src', 'database', 'migrations')
